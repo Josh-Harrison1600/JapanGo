cddfa0ca28808d25416a08ea5c5ee93f7cd3b6b2
Josh Harrison <42451270+Josh-Harrison1600@users.noreply.github.com>
2025-03-20 19:04:16 -0300

    added modal for the 'submit' button in contact.tsx

--- src/assets/pages/Contact.tsx
+++ src/assets/pages/Contact.tsx
@@ -1,12 +1,21 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { motion } from 'framer-motion';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
 
 function Contact(){
+    //States for the modal
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     const formRef = useRef<HTMLFormElement | null>(null);
     const sendEmailPLACEHOLDER = (e: React.FormEvent) => {
         e.preventDefault();
         console.log('sendEmailPLACEHOLDER');
+        handleOpen();
     }
 
     //Animation variants for the fade-up effect
@@ -14,6 +23,19 @@ function Contact(){
         hidden: { opacity: 0, y: 50 },
         visible: { opacity: 1, y: 0 },
     };
+
+    //Styling for MUI Modal
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
 
     return(
         <>
@@ -30,7 +52,7 @@ function Contact(){
                 </p>
 
             {/* Form  Fields*/}
-            <div className="max-w-lg mx-auto">
+            <div className="max-w-lg mx-auto px-4 sm:px-6">
                 <form ref={formRef} onSubmit={sendEmailPLACEHOLDER} className='space-y-6'>
 
                     {/* Your Name Section */}
@@ -97,10 +119,29 @@ function Contact(){
                             </span>
                         </button>
                     </div>
-
                 </form>
+
             </div>
-            </motion.div>
+        </motion.div>
+        {/* Success Modal */}
+        <Modal
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+            <Fade in={open}>
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2" className='text-center'>
+                    Your Message has been sent!
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }} className='text-center'>
+                    We will get back to you as soon as possible.
+                    </Typography>
+                </Box>
+            </Fade>
+            </Modal>
         </>
     )
 } 
